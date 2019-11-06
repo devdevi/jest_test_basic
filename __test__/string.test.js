de0e8d19bfd4795ba3ab06267db54dc254cc8f71
visidevi <visidevi@gmail.com>
2019-11-06 11:52:55 -0300

    watch

--- __test__/string.test.js
+++ __test__/string.test.js
@@ -1,7 +1,14 @@
 describe('Comprobar cadenas de texto', () => {
-
     const msg = 'Un bonito texto';
     test('Debe contener el siguiente texto', () => {
         expect(msg).toMatch(/bonito/);
+    })
+
+    test('NO debe contener el siguiente texto', () => {
+        expect(msg).not.toMatch("bonitos");
+    })
+
+    test('Comprobar el tamaño', () => {
+        expect(msg).toHaveLength(15);
     })
 })
